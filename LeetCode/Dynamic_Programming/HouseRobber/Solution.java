68e23e324cbece976715f2fe31a88aa44e0a785d
duduldu <duduldu@126.com>
2022-11-30 23:06:23 +0800

    update House Robber

--- LeetCode/Dynamic_Programming/HouseRobber/Solution.java
+++ LeetCode/Dynamic_Programming/HouseRobber/Solution.java
@@ -14,11 +14,13 @@ public class Solution {
         int len = nums.length;
         if(len==1) return nums[0];
         int[] res = new int[len];
-        res[0] = nums[0];
-        res[1] = Math.max(nums[0], nums[1]);
+        int a = nums[0];
+        int b = Math.max(a, nums[1]);
         for(int i=2;i<len;i++){
-            res[i]=Math.max(res[i-1],res[i-2]+nums[i]);
+            int c = Math.max(b,a+nums[i]);
+            a=b;
+            b=c;
         }
-        return res[len-1];
+        return b;
     }
 }
