446788ea11224882ebd284da6f75ea3eb82f0a61
duduldu <duduldu@126.com>
2022-12-06 23:53:24 +0800

    add Jump Game II

---
+++ LeetCode/Dynamic_Programming/JumpGameII/Solution.java
@@ -0,0 +1,14 @@
+package Dynamic_Programming.JumpGameII;
+
+import org.junit.Assert;
+
+public class Solution {
+    public static void main(String[] args) {
+        Assert.assertEquals(2, jump(new int[]{2, 3, 1, 1, 4}));
+        Assert.assertEquals(2, jump(new int[]{2, 3, 0, 1, 4}));
+    }
+
+    public static int jump(int[] nums) {
+        return 0;
+    }
+}
