6e79085b2959f92384c7f4cee38a8902bbbefd46
duduldu <duduldu@126.com>
2022-12-07 21:56:55 +0800

    update Jump Game II

--- LeetCode/Dynamic_Programming/JumpGameII/Solution.java
+++ LeetCode/Dynamic_Programming/JumpGameII/Solution.java
@@ -2,6 +2,8 @@ package Dynamic_Programming.JumpGameII;
 
 import org.junit.Assert;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         Assert.assertEquals(2, jump(new int[]{2, 3, 1, 1, 4}));
@@ -9,6 +11,17 @@ public class Solution {
     }
 
     public static int jump(int[] nums) {
-        return 0;
+        int len = nums.length;
+        int[] tot =new int[len];
+        Arrays.fill(tot,Integer.MAX_VALUE);
+        tot[0]=0;
+        for(int i =0;i<len;i++){
+            int canJump = nums[i];
+            for(int j=i+1;j<len&&canJump>0;j++){
+                tot[j]=Math.min(tot[i]+1,tot[j]);
+                canJump--;
+            }
+        }
+        return tot[len-1];
     }
 }
