06592d75b19ef7b0bae634cfe1560c28e44ca1c2
duduldu <duduldu@126.com>
2022-12-13 23:16:25 +0800

    add Maximum Subarray

---
+++ LeetCode/Dynamic_Programming/MaximumSubarray/Solution.java
@@ -0,0 +1,17 @@
+package Dynamic_Programming.MaximumSubarray;
+
+import org.junit.Assert;
+
+public class Solution {
+    public static void main(String[] args) {
+        Assert.assertEquals(6, maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        Assert.assertEquals(1, maxSubArray(new int[]{1}));
+        Assert.assertEquals(23, maxSubArray(new int[]{5,4,-1,7,8}));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int temp = nums[0];
+       return 0;
+    }
+}
