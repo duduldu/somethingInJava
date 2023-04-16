9f8405fb61a147c1ee64a87c82a9f04d92f017c5
duduldu <duduldu@126.com>
2023-04-16 22:29:34 +0800

    update Maximum Subarray

--- LeetCode/Dynamic_Programming/MaximumSubarray/Solution.java
+++ LeetCode/Dynamic_Programming/MaximumSubarray/Solution.java
@@ -2,32 +2,22 @@ package Dynamic_Programming.MaximumSubarray;
 
 import org.junit.Assert;
 
+import java.util.ArrayList;
+
 public class Solution {
     public static void main(String[] args) {
-        Assert.assertEquals(6, maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        Assert.assertEquals(6, maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
         Assert.assertEquals(1, maxSubArray(new int[]{1}));
-        Assert.assertEquals(23, maxSubArray(new int[]{5,4,-1,7,8}));
+        Assert.assertEquals(23, maxSubArray(new int[]{5, 4, -1, 7, 8}));
     }
 
     public static int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int temp = nums[0];
-        int tot[] = new int[nums.length];
-        for(int num:nums){
-            if(temp<=0){
-                if(num>=0){
-                    temp=num;
-                    max=num;
-                }
-                else {
-                    max=Math.max(temp,num);
-                    tot[0]=max;
-                }
-            }
-            else {
-                max+=num;
-            }
+        int max = nums[0];
+        int temp = max;
+        for (int i = 1; i < nums.length; i++) {
+            temp = Math.max(nums[i],nums[i]+temp);
+            max = Math.max(temp,max);
         }
-       return max;
+        return max;
     }
 }
