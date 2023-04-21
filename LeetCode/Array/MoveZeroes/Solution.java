c2ad5bfd3b86e8811c6bfe6396048255d4db03aa
duduldu <duduldu@126.com>
2023-04-21 23:05:04 +0800

    update Move Zeroes

--- LeetCode/Array/MoveZeroes/Solution.java
+++ LeetCode/Array/MoveZeroes/Solution.java
@@ -2,29 +2,31 @@ package Array.MoveZeroes;
 
 import org.junit.Assert;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public static void main(String[] args) {
-        Assert.assertArrayEquals(new int[]{1,3,12,0,0}, moveZeroes(new int[]{0,1,0,3,12}));
+        Assert.assertArrayEquals(new int[]{1, 3, 12, 0, 0}, moveZeroes(new int[]{0, 1, 0, 3, 12}));
         Assert.assertArrayEquals(new int[]{0}, moveZeroes(new int[]{0}));
     }
 
     public static int[] moveZeroes(int[] nums) {
         int count = 0;
-        for(int num:nums){
-            if(num==0) count++;
+        for (int num : nums) {
+            if (num == 0) count++;
         }
-        if(count==0) return nums;
+        if (count == 0) return nums;
         int left = 0;
         int right = 0;
-        while(right<nums.length){
-            if(nums[left]==0&&nums[right]!=0) {
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                int temp = nums[left];
                 nums[left] = nums[right];
+                nums[right] = temp;
                 left++;
-                right++;
             }
+            right++;
+        }
+        for (int i = right; i < nums.length; i++) {
+            nums[i] = 0;
         }
         return nums;
     }
